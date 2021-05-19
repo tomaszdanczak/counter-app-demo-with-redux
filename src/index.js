ea2f36678c4d548d00b99d9b7b86f78f87cf14dd
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-19 15:02:15 +0200

    feat: use store in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import counterReducer from "./reducers/counter";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
