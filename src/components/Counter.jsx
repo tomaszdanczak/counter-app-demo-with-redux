cd3d880780fe59656bf16a8c8ee6e276a98a0dbc
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-19 15:16:26 +0200

    feat: add styles to Counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,11 +5,21 @@ const Counter = (props) => {
 
   return (
     <div>
-      <span>{value}</span>
-      <button>Increment</button>
-      <button>Delete</button>
+      <span className={getBadgeClasses(value)}>{formatCount(value)}</span>
+      <button className="btn btn-secondary btn-sm">Increment</button>
+      <button className="btn btn-danger btn-sm m-2">Delete</button>
     </div>
   );
 };
 
 export default Counter;
+
+const getBadgeClasses = (value) => {
+  let classes = "badge m-2 badge-";
+  classes += value === 0 ? "warning" : "primary";
+  return classes;
+};
+
+const formatCount = (value) => {
+  return value === 0 ? "Zero" : value;
+};
