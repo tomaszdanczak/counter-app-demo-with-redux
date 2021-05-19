07922eb716a4ce33449ffe409ec5c5079640750d
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-19 15:24:19 +0200

    feat: call action reset in button reset

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "./Counter";
+import { reset } from "./../actions";
 
 const Counters = () => {
   const counters = useSelector((state) => state.counters);
+  const dispatch = useDispatch();
 
   const countersToRender = counters.map((counter) => (
     <Counter key={counter.id} id={counter.id} value={counter.value} />
@@ -11,7 +13,12 @@ const Counters = () => {
 
   return (
     <div>
-      <button className="btn btn-primary btn-sm m2">Reset</button>
+      <button
+        onClick={() => dispatch(reset())}
+        className="btn btn-primary btn-sm m2"
+      >
+        Reset
+      </button>
       {countersToRender}
     </div>
   );
