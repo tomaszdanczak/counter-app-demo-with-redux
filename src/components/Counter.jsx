e144d8d060e980bd174a1dc04f30d1974c4620eb
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-19 15:10:42 +0200

    feat: use props to create structure of Counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Counter = () => {
-  return <h2>Counter</h2>;
+const Counter = (props) => {
+  const { value } = props;
+
+  return (
+    <div>
+      <span>{value}</span>
+      <button>Increment</button>
+      <button>Delete</button>
+    </div>
+  );
 };
 
 export default Counter;
